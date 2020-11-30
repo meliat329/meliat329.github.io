91d3a07b2fa3f9e874f5d7813910475fe2023503
meliat329 <33754787+meliat329@users.noreply.github.com>
2020-11-30 12:16:51 -0600

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,53 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+// Get the header
+var header = document.getElementById("myMenuBar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+
+var aboutPos = document.getElementById("about");
+var nav = document.getElementById("myMenuBar");
+var x = 0;
+
+function scrollAbout() {
+    if (x == 0) {
+        window.scrollTo(0, (aboutPos.offsetTop - nav.offsetHeight));
+        x += 1;
+    }
+    else {
+        window.scrollTo(0, aboutPos.offsetTop);
+    }
+}
+
+var academPos = document.getElementById("academics");
+function scrollAcademics() {
+    window.scrollTo(0, academPos.offsetTop);
+}
+
+var expPos = document.getElementById("experience");
+function scrollExperience() {
+    window.scrollTo(0, expPos.offsetTop);
+}
+
+var contPos = document.getElementById("contact");
+function scrollContact() {
+    window.scrollTo(0, contPos.offsetTop);
+}
+
+var frmvalidator = new Validator("contactform");
+frmvalidator.addValidation("name", "req", "Please provide your name");
+frmvalidator.addValidation("email", "req", "Please provide your email");
+frmvalidator.addValidation("email", "email", "Please enter a valid email address");
+
